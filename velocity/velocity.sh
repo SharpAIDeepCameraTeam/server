0a7e035017494f93f88dc90bc3ec6c2d3bd0d0e6
SharpAIDeepCameraTeam <asher2020zhang@gmail.com>
2024-12-06 22:25:00 -0800

    Further reduced memory usage and simplified JVM options

--- velocity/velocity.sh
+++ velocity/velocity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 while [ true ]; do
-    java -Xms128M -Xmx128M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -jar velocity.jar
+    java -Xms64M -Xmx64M -XX:+UseG1GC -XX:G1HeapRegionSize=2M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -jar velocity.jar
 
     echo Server restarting...
     echo Press CTRL + C to stop.
